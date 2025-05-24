510ac7f025ba57bff91327880cb60dc5288e018a
sogand <sogandprh@gmail.com>
2025-05-24 05:15:09 -0700

    cat Facts Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,29 @@
-import React, {useState} from 'react';
 import styles from './App.module.css';
-import Course from "./Course.jsx";
-
-// import {useState} from "react";
+import Axios from 'axios';
+import {useEffect, useState} from "react";
 
 
 function App() {
+    const [catfact, setCatfacts] = useState("")
 
-    const [courseList, setCourseList] = useState([])
-    const [newCourse, setNewCourse] = useState("")
+    useEffect(() => {
+        fetchFact()
 
-    const handleChange = (event) => {
-        setNewCourse(event.target.value)
+    }, [])
+
+    const fetchFact = () => {
+
+        Axios.get("https://catfact.ninja/fact")
+            .then((res) => {
+                setCatfacts(res.data.fact)
+
+            })
     }
-
-    const addCourse = () => {
-        const course = {
-            id: courseList.length === 0
-                ? 1
-                : courseList[courseList.length - 1].id + 1,
-            courseName: newCourse
-        }
-        setCourseList([...courseList, course])
-    }
-
-
-    const deleteCourse = (courseId)=> {
-        setCourseList(courseList.filter((course) =>
-             courseId !== course.id )
-        )
-
-    }
-
     return (
         <div className={styles.App}>
-            <div>
-                <input type="text" onChange={handleChange}/>
-                <button onClick={addCourse}>add course</button>
-            </div>
-            <div className="list">
-                {courseList.map((course , index) => {
-                  return(
-                      <Course key={index} course={course} deleteCourse = {deleteCourse}/>
-                  )
-                })}
-            </div>
+
+            <button onClick={fetchFact}>fetch cat</button>
+            <p>{catfact}</p>
         </div>
 
     );
