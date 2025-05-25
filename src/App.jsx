8e3b50bf691a327006db50c7db9986a920bc2f21
sogand <sogandprh@gmail.com>
2025-05-25 13:48:56 -0700

    reUse toggle hook

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,14 @@ import {useToggle} from "./useToggle.jsx";
 function App() {
 
     const [isVisible , toggle]= useToggle(false)
+    const [isVisible2 , toggle2]= useToggle(false)
 
     return (
         <div className={styles.App}>
             <button onClick={toggle}> {isVisible ? "show" : "hide" }</button>
             {isVisible && <h1>"my hidden text"</h1>}
+            <button onClick={toggle2}> {isVisible2 ? "show" : "hide" }</button>
+            {isVisible2 && <h1>"my hidden text"</h1>}
 
         </div>
 
